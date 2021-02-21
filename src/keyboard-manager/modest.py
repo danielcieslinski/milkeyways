1c5d54bbd8b0042f55f1df6f86531d289f5c5be1
Daniel Cieśliński <daniel98c@gmail.com>
2021-02-21 09:52:08 +0100

    local

--- src/keyboard-manager/modest.py
+++ src/keyboard-manager/modest.py
@@ -1,10 +1,29 @@
-import keyboard
+from Xlib.display import Display
+from Xlib import X
 
-class LocalManager:
+class KeyBinder(object):
+    def __init__(self):
+        self.cancel = False
+        self.xdisp = Display()
+        self.xroot = self.xdisp.screen().root
+        self.key_code = 71 # F5
 
-    self.mappings.
-    def add(self, active: Callable, bind):
-        rules()
+    def handle_event(self, event):
+        keycode = event.detail
+        print(keycode)
+        if event.type == X.KeyPress:
+            print(">>> event captured")
+            # self.cancel = True
 
+    def start(self):
+        self.xroot.change_attributes(event_mask = X.KeyPressMask)
+        self.xroot.grab_key(self.key_code, X.AnyModifier, 1, X.GrabModeAsync, X.GrabModeAsync)
 
-keyboard.monotonic
+        while not self.cancel:
+            event = self.xroot.display.next_event()
+            self.handle_event(event)
+
+if __name__ == '__main__':
+    keybinder = KeyBinder()
+    keybinder.start()
+    keybinder.cancel = True
---
+++ src/keyboard-manager/parse.py
@@ -0,0 +1,32 @@
+
+import sh
+
+# class Action:
+    # sh.
+
+def parse():
+    pass
+
+
+
+
+"""
+# Expression: al -> another word 
+# Must require backspace
+"""
+
+# a -> b
+
+def parse(e: str):
+    """
+    :param e: 'a -> α'
+    :return:
+    """
+    return e.replace(' ', '').strip().split('->')
+
+# class Parse:
+#     '->'
+#     def one
+
+
+# hjkl
